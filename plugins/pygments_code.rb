29a8fac8d089dd4df117f48ce30dbedeb5e6f245
Geoff Schorkopf <gschorkopf@gmail.com>
2014-05-15 16:10:13 -0400

    Gives the Heroku/Octopress build pack a fighting chance
    http://jasongarber.com/blog/2012/01/10/deploying-octopress-to-heroku-wit
    h-a-custom-buildpack/

--- plugins/pygments_code.rb
+++ plugins/pygments_code.rb
@@ -1,7 +1,9 @@
-require 'pygments'
+require 'net/http'
+require 'uri'
 require 'fileutils'
 require 'digest/md5'
 
+PYGMENTIZE_URL = URI.parse('http://pygmentize.herokuapp.com/')
 PYGMENTS_CACHE_DIR = File.expand_path('../../.pygments-cache', __FILE__)
 FileUtils.mkdir_p(PYGMENTS_CACHE_DIR)
 
@@ -22,14 +24,14 @@ module HighlightCode
         highlighted_code = File.read(path)
       else
         begin
-          highlighted_code = Pygments.highlight(code, :lexer => lang, :formatter => 'html', :options => {:encoding => 'utf-8', :startinline => true})
+          highlighted_code = Net::HTTP.post_form(PYGMENTIZE_URL, {'lang'=>lang, 'code'=>code}).body
         rescue MentosError
           raise "Pygments can't parse unknown language: #{lang}."
         end
         File.open(path, 'w') {|f| f.print(highlighted_code) }
       end
     else
-      highlighted_code = Pygments.highlight(code, :lexer => lang, :formatter => 'html', :options => {:encoding => 'utf-8', :startinline => true})
+      highlighted_code = Net::HTTP.post_form(PYGMENTIZE_URL, {'lang'=>lang, 'code'=>code}).body
     end
     highlighted_code
   end
